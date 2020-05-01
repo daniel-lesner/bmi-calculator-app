9ebdaff20f5297b50c55d5f8cae106e03d54e962
daniel-lesner <44841410+daniel-lesner@users.noreply.github.com>
2020-05-01 23:02:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 function myFunction(){
+    let bmi=BMI();
+
+    if (isNaN (bmi)){
+        alert("Please complete both boxes with numeric values")
+        return
+    }
+    
+    let result = getResult(bmi);
+    printResult(result);
+   
+    function getResult(bmi) {
+      let resultMap = [
+        {
+          cond: (bmi) => bmi < 18.5,
+          text: "underweight",
+        },
+        {
+          cond: (bmi) => bmi >= 18.5 && bmi < 25,
+          text: "fine",
+        },
+        {
+          cond: (bmi) => bmi >= 25 && bmi < 30,
+          text: "overweight",
+        },
+        {
+          cond: (bmi) => bmi >= 30,
+          text: "obese",
+        }
+      ];
+     
+      return resultMap.reduce((result, variant) => result + (variant.cond(bmi) ? variant.text : ""), "");
+    }
+   
+    function printResult(result) {
+      let box3 = document.getElementById("box3");
+      box3.innerHTML = `You are ${result}, your BMI is ${bmi.toFixed(2)}`;
+      box3.style.display = "block";
+    }
+
+
+    function BMI() {
     let weight=document.getElementById("weight").value;
     let height=document.getElementById("height").value/100;
-    let bmi=weight/(height**2);
-    
-    if (bmi<18.5){
-        document.getElementById("box3").innerHTML = "You are underweight, your BMI is " + bmi.toFixed(2);
-        document.getElementById("box3").style.display = "block";
-    } 
-    else if ((bmi>=18.5) && (bmi<25)){
-        document.getElementById("box3").innerHTML = "You are fine, your BMI is " + bmi.toFixed(2);
-        document.getElementById("box3").style.display = "block";
+    return weight/(height**2);
+    }
 
-    } 
-    else if ((bmi >=25) && (bmi <30)){
-        document.getElementById("box3").innerHTML = "You are overweight, your BMI is " + bmi.toFixed(2);
-        document.getElementById("box3").style.display = "block";
-    } 
-    else if (bmi>=30){
-        document.getElementById("box3").innerHTML = "You are obese, your BMI is " + bmi.toFixed(2);
-        document.getElementById("box3").style.display = "block";
-    }
-    else {
-        alert("Please complete both boxes with numeric values")
-    }
 }
 
