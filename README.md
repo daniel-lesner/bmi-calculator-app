dea9389378bedfece420e1abcdaab2e31a57d44f
Lesner <Daniel.Lesner@gfk.com>
2020-08-05 23:54:20 +0300

    Rename files, added assets folder, modified CSS

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Responsive Body Mass Index Calculator application made with Vanilla JavaScript.
 
 You can access the website at: https://daniel-lesner.github.io/BMICalculator/
+
+To Do List:\
+Add button shadow\
+Add buttons to choose other measurement units\
+Add different text colors for the message, depending on the BMI value
---
+++ assets/script.js
@@ -0,0 +1,47 @@
+function myFunction(){
+    let bmi=BMI();
+
+    if (isNaN (bmi)){
+        alert("Please complete both boxes with numeric values")
+        return
+    }
+    
+    let result = getResult(bmi);
+    printResult(result);
+   
+    function getResult(bmi) {
+      let resultMap = [
+        {
+          cond: (bmi) => bmi < 18.5,
+          text: "underweight",
+        },
+        {
+          cond: (bmi) => bmi >= 18.5 && bmi < 25,
+          text: "fine",
+        },
+        {
+          cond: (bmi) => bmi >= 25 && bmi < 30,
+          text: "overweight",
+        },
+        {
+          cond: (bmi) => bmi >= 30,
+          text: "obese",
+        }
+      ];
+     
+      return resultMap.reduce((result, variant) => 
+      result + (variant.cond(bmi) ? variant.text : ""), "");
+    }
+   
+    function printResult(result) {
+      let box3 = document.getElementById("box3");
+      box3.innerHTML = `You are ${result}, your BMI is ${bmi.toFixed(2)}`;
+      box3.style.display = "block";
+    }
+
+    function BMI() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value / 100;
+    return weight / (height ** 2);
+    }
+}
